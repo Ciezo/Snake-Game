6a0c5272a3844b4605b9d19728f9dce411f91ccb
Ciezo <74012129+Ciezo@users.noreply.github.com>
2021-10-14 15:18:28 +0800

    Add files via upload
    Create a repository to store files and to link this to the project.

---
+++ Snake-Game/src/SnakeGame_window.java
@@ -0,0 +1,15 @@
+import javax.swing.*; 
+import java.awt.*;
+import java.awt.event.*;
+
+// PACKAGE SECTION 
+
+
+
+// IMPORT SECTION 
+
+
+
+public class SnakeGame_window extends JFrame implements ActionListener {
+    
+}
