cf76ef9d425fa4bd5d547636f8a70f5749043f33
Cloyd Van <74012129+Ciezo@users.noreply.github.com>
2021-10-17 22:11:46 +0800

    Update and added main method
    Updated this core file as well as added a main method to test and run. Additionally, some functions and attributes have been fixed as well.

--- Snake-Game/src/SnakeProto.java
+++ Snake-Game/src/SnakeProto.java
@@ -44,22 +44,22 @@ public class SnakeProto extends JPanel implements ActionListener{
     public void draw(Graphics g){
         if(running) {
 
-        
+
             for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++){
                 g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
                 g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
-         }
+            }
             g.setColor(Color.red);
             g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-        
+
             for(int i = 0;i < bodyParts;i++) {
                 if(i == 0) {
-                 g.setColor(Color.green);
+                    g.setColor(Color.green);
                     g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-             }
+                }
                 else {
-                 g.setColor(new Color(45,180,0));
-                 g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                    g.setColor(new Color(45,180,0));
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
                 }
             }
             g.setColor(Color.red);
@@ -76,7 +76,7 @@ public class SnakeProto extends JPanel implements ActionListener{
         appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
         appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
     }
-   
+
     public void move(){
         for(int i = bodyParts;i>0;i--){
             x[i] = x[i-1];
@@ -89,13 +89,13 @@ public class SnakeProto extends JPanel implements ActionListener{
                 break;
             case 'D':
                 y[0] = y[0] + UNIT_SIZE;
-                break; 
+                break;
             case 'L':
                 x[0] = x[0] - UNIT_SIZE;
-                break; 
+                break;
             case 'R':
                 x[0] = x[0] + UNIT_SIZE;
-                break;      
+                break;
         }
     }
 
@@ -145,11 +145,11 @@ public class SnakeProto extends JPanel implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+
         if(running) {
             move();
             checkApple();
-            checkCollisions();  
+            checkCollisions();
         }
         repaint();
     }
@@ -181,8 +181,21 @@ public class SnakeProto extends JPanel implements ActionListener{
             }
         }
     }
-    
+
+    static class GameFrame extends JFrame {
+
+        GameFrame() {
+            this.add(new SnakeProto());
+            this.setTitle("SnakeGame");
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.pack();
+            this.setVisible(true);
+            this.setLocationRelativeTo(null);
+        }
+
+    }
+
     public static void main(String[] args) {
-        new SnakeProto();
+        new GameFrame();
     }
 }
