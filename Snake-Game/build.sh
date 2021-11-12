09d76c00f98cc665a9dfe11306381ac803c87e4e
Cloyd Van <74012129+Ciezo@users.noreply.github.com>
2021-11-12 18:53:21 +0800

    Tweaks and minor improvements
    Edits and fixes for the build and compilation script

--- Snake-Game/build.sh
+++ Snake-Game/build.sh
@@ -10,4 +10,10 @@ echo -e "███████████████████████�
 echo -e "Attempting to build an executable file.    ::      Format ==> .jar"
 echo -e "██████████████████████████████████████████████████████████████████"
 mkdir "release"
-jar -cvfe release/mmmmmSnekkk_v0.1.jar src/Main bin/Main.class bin/*.class src bin assets sfxpack 
+echo -e " \t ==> Folder created!"
+jar -cvfe release/mmmmmSnekkk_v0.1.jar src/Main bin/Main.class bin/*.class src bin assets sfxpack
+jar -cvfe mmmmmSnekkk_v0.1.jar src/Main bin/Main.class bin/*.class src bin assets sfxpack
+echo -e "\n \n A copy of generated executable file is in ==>   release folder!"
+echo -e "---------------------------------------------------------------------"
+echo -e "\n \n Generated generated executable file is in ==>   / folder!"
+echo -e "---------------------------------------------------------------------"
