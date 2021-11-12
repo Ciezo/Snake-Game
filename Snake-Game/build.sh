6112a844c0af37bb516b3e20807619cc25637d40
Cloyd Van <74012129+Ciezo@users.noreply.github.com>
2021-11-12 12:57:06 +0800

    Update to the build script
    Some tweaks and attempt to make a real executable jar file using this script

--- Snake-Game/build.sh
+++ Snake-Game/build.sh
@@ -1,9 +1,12 @@
+echo -e "**** Redirecting to src ****"
 cd src/
+echo -e "**** Recompiling source files ****"
 sh ./compile.sh
+echo -e "**** Redirecting to / ****"
 cd ../
-jar -cvfe SnakeGame.jar src bin assets sfxpack 
 
-
-
-
-
+echo -e "\n \n"
+echo -e "██████████████████████████████████████████████████████████████████"
+echo -e "Attempting to build an executable file.    ::      Format ==> .jar"
+echo -e "██████████████████████████████████████████████████████████████████"
+jar -cvfe mmmmmSnekkk_v0.1.jar src/Main.java src bin bin/Main*.class assets sfxpack 
